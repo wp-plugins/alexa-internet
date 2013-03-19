3a1cb09c20a8f5afe12ef2204d8d4df1eea45128
alexainternet <alexainternet@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-19 21:35:45 +0000

    Major updates throughout
    git-svn-id: https://plugins.svn.wordpress.org/alexa-internet/trunk@684428 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- alexacertify_admin.php
+++ alexacertify_admin.php
@@ -1,58 +1,24 @@
 <?php
-$updated = false;
-$error = array();
-$certify = "";
-$verify = "";
-
 //must check that the user has the required capability 
 if (!current_user_can('manage_options')) {
     wp_die( __('You do not have sufficient permissions to access this page.') );
 }
-
-if (isset($_POST["alexacertify_hidden"]) && $_POST["alexacertify_hidden"] == "Y") {
-	// Form set
-	$certify = trim(stripslashes($_POST["alexacertify_certify"]));
-	$verify = trim(stripslashes($_POST["alexacertify_verify"]));
-
-	if ($certify) {
-		if (!preg_match("|<script [^>]*>[^<]*</script><script [^>]*>[^<]*</script><noscript><img [^>]*></noscript>|", $certify)) {
-			$error["certify"] = "Invalid Certify Code";
-		}
-	}
-
-	if ($verify) {
-	    if (!preg_match('/^([^\'"])+$/', $verify)) {
-	        $error["verify"] = "Invalid Verifcation ID";
-	    }
-	}
-
-	if (!count($error)) {
-		update_option('alexacertify_certify', $certify);
-		update_option('alexacertify_verify', $verify);
-		$updated = true;
-	}
-} else {
-	// Normal display
-	$certify = get_option("alexacertify_certify");
-	$verify = get_option("alexacertify_verify");
-}
-
 $formAction = str_replace( '%7E', '~', $_SERVER['REQUEST_URI']);
 ?>
 
-<?php if ($updated): ?>
-    <div class="updated"><p><strong><?php _e('Options saved.' ); ?></strong></p></div>
+<?php if (!empty($this->updated)): ?>
+    <div class="updated"><?= implode("<br />\n", $this->updated) ?></div>
 <?php endif;?>   
 
-<?php if (!empty($error)): ?>
-    <div class="error"><?= implode("<br />\n", $error) ?></div>
+<?php if (!empty($this->errors)): ?>
+    <div class="error"><?= implode("<br />\n", $this->errors) ?></div>
 <?php endif; ?>
 
 <div class="wrap">
     <h2> The Official Alexa WordPress Plugin </h2>
 
 	<form name="alexacertify_form" method="post" action="<?php echo $formAction ?>">
-        <input type="hidden" name="alexacertify_hidden" value="Y"/>
+        <input type="hidden" name="alexacertify_submit" value="Y"/>
         
         <h3> Claim Your Site </h3>
         <p>
@@ -65,7 +31,7 @@ $formAction = str_replace( '%7E', '~', $_SERVER['REQUEST_URI']);
 	    <p>
 	        Verification ID:
             <br/>
-	        <input type="text" name="alexacertify_verify" value="<?= htmlspecialchars($verify) ?>" size="40">
+	        <input type="text" name="alexacertify_verify" value="<?= htmlspecialchars($this->verify_tag) ?>" size="40">
             <br/>
             example: kbgNFC70SGDf0pmgwT0VGoKlRtI
         </p>
@@ -77,7 +43,7 @@ $formAction = str_replace( '%7E', '~', $_SERVER['REQUEST_URI']);
         <p>
             Alexa Pro customers have the opportunity to have their website traffic
             Certified by Alexa. To learn more, visit Alexa's 
-            <a href="http://www.alexa.com/products" target="_blank">professional products</a> page.
+            <a href="http://www.alexa.com/siteowners/certify" target="_blank">Certified Site Metrics</a> page.
 	        <br/>
 	        If you are an Alexa Pro customer, paste your Certify Code into the box
 	        below and the Certification process will begin shortly.
@@ -85,7 +51,7 @@ $formAction = str_replace( '%7E', '~', $_SERVER['REQUEST_URI']);
         <p>
 	        Certify Code:
             <br />
-            <textarea name="alexacertify_certify" style="width: 80%; height: 8em;"><?= htmlspecialchars($certify) ?></textarea>
+            <textarea name="alexacertify_certify" style="width: 80%; height: 8em;"><?= htmlspecialchars($this->certify_snippet) ?></textarea>
         </p>
         <p class="submit">  
            <input type="submit" name="Submit" value="Update Options" />  
--- readme.txt
+++ readme.txt
@@ -12,25 +12,27 @@ This plugin allows you to easily claim and ceritfy your Wordpress blog on Alexa.
 
 The Official Alexa Wordpress Plugin makes it easy to claim your Wordpress site on Alexa.com. Once you have claimed your site, you can edit your Alexa.com site listing.
 
-If you have an Alexa Pro subscription for your Wordpress site, this plugin makes it easy to add the Certify Code. To learn more about Certification and Alexa Pro, visit http://www.alexa.com/products
+If you have an Alexa Pro subscription for your Wordpress site, this plugin makes it easy to add the Certify Code. To learn more about Certification and Alexa Pro, visit http://www.alexa.com/siteowners/certify
 
 == Installation ==
 
 To claim your site:
-
 1. Upload the Official Alexa Wordpress Plugin to your site.
-2. Activate the plugin through the 'Plugins' menu in WordPress
-3. Visit http://www.alexa.com/siteowners/claim and follow the instructions
-4. Enter your Alexa verification ID into the plugin and Update
-5. Click "Verify my ID" on Alexa.com
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Visit http://www.alexa.com/siteowners/claim and follow the instructions
+1. Enter your Alexa verification ID into the plugin and Update
+1. Click "Verify my ID" on Alexa.com
 
 To certify your site:
-
 1. Upload the Official Alexa Wordpress Plugin to your site.
-2. Activate the plugin through the 'Plugins' menu in WordPress
-3. Visit http://www.alexa.com/pro/dashboard and follow the instructions
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Visit http://www.alexa.com/pro/dashboard and follow the instructions
 
 
 == Changelog ==
+
+= 2.0 =
+
+Major revisions throughout
 
 = 1.0 =
